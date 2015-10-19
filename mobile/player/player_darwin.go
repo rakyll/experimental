58d85ccbd4d1652296d69c2f0c3c8d0785ed2b73
Burcu Dogan <jbd@google.com>
2015-10-19 12:27:22 -0700

    Add play.

--- mobile/player/player_darwin.go
+++ mobile/player/player_darwin.go
@@ -4,19 +4,31 @@ package player
 #cgo CFLAGS: -x objective-c
 #cgo LDFLAGS: -framework Foundation -framework AVFoundation
 
+#import <stdlib.h>
+
 #import <AVFoundation/AVPlayer.h>
 #import <CoreFoundation/CoreFoundation.h>
 
 AVPlayer* newPlayer(char* url);
+void play(AVPlayer*);
 
 */
 import "C"
+
+import "unsafe"
 
 type Player struct {
 	p *C.struct_AVPlayer
 }
 
 func NewPlayer(url string) *Player {
-	p := C.newPlayer(nil)
+	cu := C.CString(url)
+	defer C.free(unsafe.Pointer(cu))
+
+	p := C.newPlayer(cu)
 	return &Player{p: p}
+}
+
+func (p *Player) Play() {
+	C.play(p.p)
 }
--- mobile/player/player_darwin.m
+++ mobile/player/player_darwin.m
@@ -5,3 +5,7 @@ AVPlayer* newPlayer(char* url) {
   NSURL* u = [NSURL URLWithString: [NSString stringWithCString:url encoding:NSASCIIStringEncoding]];
   return [AVPlayer playerWithURL: u];
 }
+
+void play(AVPlayer* p) {
+  [p play];
+}
