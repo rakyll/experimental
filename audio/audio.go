fbaf5e00199aaa81158a976a1caf1bd3a38e381d
Burcu Dogan <jbd@google.com>
2015-11-05 13:03:44 -0800

    Remove godoc.

--- audio/audio.go
+++ audio/audio.go
@@ -85,73 +85,48 @@ var codeToState = map[int32]State{
 	al.Stopped: Stopped,
 }
 
-// Player is a basic audio player that plays PCM data.
-// Operations on a nil *Player are no-op, a nil *Player can
-// be used for testing purposes.
 type Player struct{}
 
-// NewPlayer returns a new Player.
-// It initializes the underlying audio devices and the related resources.
-// If zero values are provided for format and sample rate values, the player
-// determines them from the source's WAV header.
-// An error is returned if the format and sample rate can't be determined.
-//
-// The audio package is only designed for small audio sources.
 func NewPlayer(src ...Stream) (*Player, error) {
 	panic("not yet implemented")
 }
 
-// Play buffers the source audio to the audio device and starts
-// to play the source.
-// If the player paused or stopped, it reuses the previously buffered
-// resources to keep playing from the time it has paused or stopped.
 func (p *Player) Play() error {
 	panic("not yet implemented")
 }
 
-// Pause pauses the player.
 func (p *Player) Pause() error {
 	panic("not yet implemented")
 }
 
-// Stop stops the player.
 func (p *Player) Stop() error {
 	panic("not yet implemented")
 }
 
-// Seek moves the play head to the given offset relative to the start of the source.
 func (p *Player) Seek(offset time.Duration) error {
 	panic("not yet implemented")
 }
 
-// Current returns the current playback position of the audio that is being played.
 func (p *Player) Current() time.Duration {
 	panic("not yet implemented")
 }
 
-// Total returns the total duration of the audio source.
 func (p *Player) Total() time.Duration {
 	panic("not yet implemented")
 }
 
-// Volume returns the current player volume. The range of the volume is [0, 1].
 func (p *Player) Volume() float64 {
 	panic("not yet implemented")
 }
 
-// SetVolume sets the volume of the player. The range of the volume is [0, 1].
 func (p *Player) SetVolume(vol float64) {
 	panic("not yet implemented")
 }
 
-// State returns the player's current state.
 func (p *Player) State() State {
 	panic("not yet implemented")
 }
 
-// Close closes the device and frees the underlying resources
-// used by the player.
-// It should be called as soon as the player is not in-use anymore.
 func (p *Player) Close() error {
 	panic("not yet implemented")
 }
