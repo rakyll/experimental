b4a8d8d8ade5e4acff277f5a3ac7bb1093b89b94
Burcu Dogan <jbd@google.com>
2015-11-16 12:48:56 -0800

    Add sample decoder.

--- audio/example_test.go
+++ audio/example_test.go
@@ -3,15 +3,36 @@ package audio_test
 import "github.com/rakyll/experimental/audio"
 
 func Example() {
-	var s audio.Stream
-	d := audio.NewMP3Decoder(s)
-	d2 := audio.NewDecoder(d.Output())
-	d3 := audio.NewDecoder(d2.Output())
+	var r io.Reader
+	s, err := Decode(r)
+	if err != nil {
+		panic(err)
+	}
 
 	// decoding begins when d3.Ouput needs to be consumed.
-	p, err := audio.NewPlayer(d3.Output())
+	p, err := audio.NewPlayer(s)
 	if err != nil {
 		panic(err)
 	}
 	p.Play()
+
+}
+
+func Decode(r io.Reader) Stream {
+	return decodedStream{in: r}
+}
+
+type decodedStream struct {
+	in io.Reader
+
+	current int64  // current is the current frame offset. It is the number of all consumed frames.
+	buf     []byte // TODO(jbd): Buffer is a small buffer to be used for prefetching and caching.
+}
+
+func (s *decodedStream) Read(offset int64, max int64) (buf []byte, n int, err error) {
+	panic("not yet implemented")
+}
+
+func (s *decodedStream) Info() (FrameInfo, error) {
+	return FrameInfo{2, 16, 44000}, nil
 }
