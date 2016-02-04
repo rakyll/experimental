a31ce97225285d7b10daed261d2e976235fe9424
Burcu Dogan <jbd@google.com>
2016-02-03 19:59:35 -0800

    Draw white.

--- spi/example/main.go
+++ spi/example/main.go
@@ -16,7 +16,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/rakyll/experimental/spi"
 )
@@ -39,25 +38,27 @@ func main() {
 	if err := d.SetBitsPerWord(8); err != nil {
 		panic(err)
 	}
-	drawRainbow(d, 0.7)
+	draw(d, 0.7)
 }
 
 // n is the number of LEDs in the strip.
 const n = 50
 
-func drawRainbow(dev *spi.Device, freq float64) {
-	payload := make([]byte, n*4+n/2+1)
+func draw(dev *spi.Device, freq float64) {
+	payload := make([]byte, 4*n+2*n+1)
 	payload[0] = 0
 	payload[1] = 0
 	payload[2] = 0
 	payload[3] = 0
 
 	for i := 0; i < n; i++ {
-		v := float64(i)
-		payload[(i+1)*4] = 0xff                                 // alpha
-		payload[(i+1)*4+1] = byte(math.Sin(freq*v+0)*127 + 128) // red
-		payload[(i+1)*4+2] = byte(math.Sin(freq*v+2)*127 + 128) // green
-		payload[(i+1)*4+3] = byte(math.Sin(freq*v+4)*127 + 128) // blue
+		payload[(i+1)*4] = 0xff   // alpha
+		payload[(i+1)*4+1] = 0xff // red
+		payload[(i+1)*4+2] = 0xff // green
+		payload[(i+1)*4+3] = 0xff // blue
+	}
+	for i := 0; i < 2*n; i++ {
+		payload[4*n+1+i] = 0xff
 	}
 	if err := dev.Do(payload, 0); err != nil {
 		panic(err)
