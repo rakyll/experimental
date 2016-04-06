f6b55a769e31d36b6f0e34447c10b17304064960
Burcu Dogan <jbd@google.com>
2016-04-06 14:15:19 -0700

    Document i2c.go.

--- i2c/i2c.go
+++ i2c/i2c.go
@@ -1,21 +1,34 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package i2c allows users to read from an write to an I2C device.
 package i2c
 
 import (
 	"github.com/rakyll/experimental/i2c/driver"
 )
 
+// Device represents an I2C device. Devices must be closed once
+// they are no longer in use.
 type Device struct {
 	conn driver.Conn
 }
 
-func (d *Device) Read(buf []byte) (int, error) {
+// Read reads at most len(buf) number of bytes from the device. n represents
+// the total number of bytes read.
+func (d *Device) Read(buf []byte) (n int, err error) {
 	return d.conn.Read(buf)
 }
 
-func (d *Device) Write(buf []byte) (int, error) {
+// Write writes to a device the give byte buffer. n represents the total
+// number of bytes written.
+func (d *Device) Write(buf []byte) (n int, err error) {
 	return d.conn.Write(buf)
 }
 
+// Close closes the devices and releases the underlying sources.
+// All devices must be closed once they are no longer in use.
 func (d *Device) Close() error {
 	return d.conn.Close()
 }
