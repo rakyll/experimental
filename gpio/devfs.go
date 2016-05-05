fdf49b980bd439469bc5af3c8da02d380641e7bd
Jaana Burcu Dogan <jbd@google.com>
2016-05-05 12:44:34 -0700

    gpio: devfs need to be a driver.Opener not a driver.Conn

--- gpio/devfs.go
+++ gpio/devfs.go
@@ -1,5 +1,7 @@
 package gpio
 
+import "github.com/rakyll/experimental/gpio/driver"
+
 type Devfs struct {
 	// BaseAddr represents the base memory address of the BCM2835 controller.
 	// Given the zero value, Devfs will try to find the correct value by looking
@@ -7,18 +9,24 @@ type Devfs struct {
 	BaseAddr uint64
 }
 
-func (d *Devfs) Value(pin int) (int, error) {
+func (d *Devfs) Open() (driver.Conn, error) {
 	panic("not implemented")
 }
 
-func (d *Devfs) SetValue(pin int, v int) error {
+type devfsConn struct{}
+
+func (d *devfsConn) Value(pin int) (int, error) {
 	panic("not implemented")
 }
 
-func (d *Devfs) Configure(pin int, direction, active, edgeTrigger string) error {
+func (d *devfsConn) SetValue(pin int, v int) error {
 	panic("not implemented")
 }
 
-func (d *Devfs) Close() error {
+func (d *devfsConn) Configure(pin int, direction, active, edgeTrigger string) error {
+	panic("not implemented")
+}
+
+func (d *devfsConn) Close() error {
 	panic("not implemented")
 }
